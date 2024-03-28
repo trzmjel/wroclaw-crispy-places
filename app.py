adcf1581388b7877757295800d1d36b0e886a275
trzmjel <orgimi@protonmail.com>
2024-03-28 06:29:13 +0100

    Make server use https
    Right now it's Dummy certificate, might want to upgrade self-signed for
    more information for end user.

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def app_login():
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(13)
-    app.run(debug=True,host='0.0.0.0', port=8000)
+    app.run(debug=True,host='0.0.0.0', port=8000, ssl_context='adhoc')
